b4fe02399d3bab619796ac0d4a6786f06d8c6b69
John Zonneveld <jjczon@gmail.com>
2020-03-31 16:15:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,7 +95,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbq_arr = []
+  holiday_hash.each do |season, holiday_day_hash|
+    holiday_day_hash.each do |holiday_day, supplies|
+      (supplies.include? == BBQ) ? bbq_arr << holiday_day
+    end
+  end
 end
 
 
