698cb8bf4be75d72410383957959936c574057f9
John Zonneveld <jjczon@gmail.com>
2020-03-31 15:13:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,56 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash.each do |season, holiday|
+    if season == :summer
+      holiday.each do |holiday_day, data|
+        if holiday_day == :fourth_of_july
+          return data[1]
+        end
+      end
+    end
+  end
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+ holiday_hash.each do |season, holiday|
+    if season == :winter
+      holiday.each do |holiday_day, data|
+        data << supply
+      end
+    end
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+holiday_hash.each do |season, holiday|
+    if season == :spring
+      holiday.each do |holiday_day, data|
+        data << supply
+      end
+    end
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = []
+  ## binding.pry
+  supply_array.each do |item|
+    holiday_hash[season][holiday_name] << item
+  end
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,7 +78,15 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday_day_hash|
+    puts "#{season.capitalize}:"
+    
+     holiday_day_hash.each {|holiday, supplies| print "  #{key.capitalize}:"}
+     ## binding.pry
+     for (i=0, i<supplies.length, i++)
+       print supplies[i]
+     end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
