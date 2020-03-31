35e50df9461664d7196fde3c040fd700440a5eeb
John Zonneveld <jjczon@gmail.com>
2020-03-31 15:19:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,10 +81,6 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_day_hash|
     puts "#{season.capitalize}:"
     
-     holiday_day_hash.each do|holiday, supplies| print "  #{key.capitalize}:"
-         print supplies
-       end
-     
   end
 end
 
